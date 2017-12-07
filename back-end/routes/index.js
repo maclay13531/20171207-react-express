c59fb9868f55a1fac67ca98000024fa09fd138ca
Jong Park <maclay13531@gmail.com>
2017-12-07 12:11:26 -0500

    updated

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var config = require('../config/config');
+var connection = mysql.createConnection(config)
+connection.connect();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/getStudents', function(req, res, next) {
+	const selectQuery = `SELECT * FROM students`;
+	connection.query(selectQuery,(error, results)=>{
+		if (error){
+			throw error;
+		}else{
+			res.json(results);		
+		}
+	})	
 });
+
+router.post('/addStudent',(req, res)=>{
+	res.json(req.body);
+})
+
 
 module.exports = router;
